edb6f17b0eeb06fd34eaa5cb339b71992cb74be3
BenjaminSwanzy <benjaminkwekunti@gmail.com>
2023-03-30 21:23:42 +0000

    update 3

--- lesson_projects/script.js
+++ lesson_projects/script.js
@@ -1,11 +1,26 @@
 let js = "amazing";
 console.log(34 - 75 + 89);
-console.log(js) 
+console.log(js)
 
 let now = 2023
 let myAge = now - 2004
 let herAge = now - 2000
 console.log(myAge)
-console.log (herAge + myAge)
+console.log(herAge + myAge)
 const avgAge = herAge + myAge / 2
 console.log(avgAge)
+
+const mBmia = 78 / (1.69 * 1.69)
+const jBmia = 92 / (1.95 * 1.95)
+console.log(mBmia, jBmia)
+if (mBmia < jBmia) {
+    console.log(`Mark has a lower bmi`)
+}else console.log(`John has a lower bmi`)
+
+
+const mBmib = 95 / (1.69 * 1.88)
+const jBmib = 85 / (1.95 * 1.76)
+console.log(mBmib, jBmib)
+if (mBmib > jBmib) {
+    console.log(`Mark has a higher bmi`)
+}else console.log(`John has a higher bmi`)
