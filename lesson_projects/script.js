94d0282fe2fce566b7bcbe94a1d6d8fd81be1294
BenjaminSwanzy <benjaminkwekunti@gmail.com>
2023-04-03 17:15:43 +0000

    gym competition

--- lesson_projects/script.js
+++ lesson_projects/script.js
@@ -1,49 +1,47 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(34 - 75 + 89);
-console.log(js)
+console.log(js)*/
 
-let now = 2023
+/*let now = 2023
 let myAge = now - 2004
 let herAge = now - 2000
 console.log(myAge)
 console.log(herAge + myAge)
 const avgAge = herAge + myAge / 2
-console.log(avgAge)
+console.log(avgAge)*/
 
-const mBmia = 78 / (1.69 * 1.69)
+/*const mBmia = 78 / (1.69 * 1.69)
 const jBmia = 92 / (1.95 * 1.95)
 console.log(mBmia, jBmia)
 if (mBmia < jBmia) {
     console.log(`Mark has a lower bmi`)
 } else {
     console.log(`John has a lower bmi`)
-}
+}*/
 
 
-const mBmib = 95 / (1.88 ** 2)
+/*const mBmib = 95 / (1.88 ** 2)
 const jBmib = 85 / (1.76 ** 2)
 console.log(mBmib, jBmib)
 if (mBmib < jBmib) {
     console.log(`Mark has a lower bmi`)
 } else {
     console.log(`John has a lower bmi`)
-}
+}*/
 
-console.log(`I am 23 years old`)
-
-const hasDrLc = true
+/*const hasDrLc = true
 const hasGdVs = true
 const isTired = true
 console.log(hasDrLc && hasGdVs);
 console.log(hasDrLc || hasGdVs);
-console.log(!hasGdVs);
+console.log(!hasGdVs);*/
 
-if (hasDrLc && hasGdVs && !isTired) {
+/*if (hasDrLc && hasGdVs && !isTired) {
     console.log(`sara fi drive`)
 } else {
     console.log(`sara no go drive`)
 }
-let favNum = Number(prompt(`what's yo fave number dude?`))
+ let favNum = Number(prompt(`what's yo fave number dude?`))
 
 if (favNum === 24) {
     console.log(`that's lit dawg it's mine too`)
@@ -51,5 +49,16 @@ if (favNum === 24) {
     console.log(`keep it below 50 dawg`)
 } else {
     console.log(`cool don ${favNum} is a vibe`)
-}
+}*/
 
+const Dolphins = (96 + 108 + 89) / 3
+const Koalas = (88 + 91 + 110) / 3
+console.log(`At the end of the first round, The Dolphins had ${Dolphins} points and The Koalas had ${Koalas} points`)
+
+if (Koalas > Dolphins) {
+    console.log(`The Koalas Win By ${Koalas} points`)
+} else if (Dolphins > Koalas) {
+    console.log(`The Dolphins win by ${Dolphins} points`)
+} else if (Koalas = Dolphins) {
+    console.log(`Laidies and Gentlemen we have a tie`)
+}
