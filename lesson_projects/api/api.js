bb13c32a378ba3db327d9f7156466ad71890e288
BenjaminSwanzy <benjaminkwekunti@gmail.com>
2023-04-12 15:20:44 +0000

    express

---
+++ lesson_projects/api/api.js
@@ -0,0 +1,19 @@
+const express = required('express')
+const router = express.router()
+
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(2400, () => {
+    console.log(`app launched on port 2400`)
+})
