20a70235b0c6b19e8b567a828faa931b11748ad9
BenjaminSwanzy <benjaminkwekunti@gmail.com>
2023-04-13 12:47:45 +0000

    added a search and delete route

--- lesson_projects/blog-api/respositories/post.repository.js
+++ lesson_projects/blog-api/respositories/post.repository.js
@@ -1,5 +1,5 @@
 
-const posts = [];//sekof we no dey use data base this stores the every post (which is an object) in this array
+let posts = [];//sekof we no dey use data base this stores the every post (which is an object) in this array
 
 class PostRepository {
     static store(data) {
@@ -14,6 +14,15 @@ class PostRepository {
         const post = posts.find((item) => item.slug === slug)
         return post
     }//to find a specific post using the slug and returns it
+
+    static findIndex(uuid) {
+        const index = posts.findIndex(item => item.uuid === uuid)
+    }//finds an index for id in case a post finna be deleted
+
+    static delete(index) {
+        posts.splice(index, 1)
+    }
+
 
 }
 //we created an fx called store; static store part => adds to the array of posts(const posts no!). return post part => searches the array(const posts) for the newly added post using the uuid amd then returns the post
