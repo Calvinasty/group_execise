79d9b7b2833f6db91abfea3998d6d1d0876aa804
BenjaminSwanzy <benjaminkwekunti@gmail.com>
2023-04-13 09:22:51 +0000

    comments for understanding added

--- lesson_projects/blog-api/respositories/post.repository.js
+++ lesson_projects/blog-api/respositories/post.repository.js
@@ -1,11 +1,11 @@
 
-const posts = [];
+const posts = [];//sekof we no dey use data base this stores the every post (which is an object) in this array
 
 class PostRepository {
     static store(data) {
         posts.push(data);
         return posts.find(post => post.uuid === data.uuid);
     }
-}
+}//we created an fx called store; static store part => adds to the array of posts. return post part => searches the array(const posts) for the newly added post using the uuid amd then returns the post
 
 module.exports = PostRepository
--- lesson_projects/blog-api/services/post.service.js
+++ lesson_projects/blog-api/services/post.service.js
@@ -1,11 +1,10 @@
 const PostRepository = require('../respositories/post.repository')
-const crypto = require('crypto');
+const crypto = require('crypto');//imported to generate uuid
 
 class PostService {
-
     static store(data) {
-        const uuid = crypto.randomUUID();
-        const slug = data['title'].split(' ').join('-')
+        const uuid = crypto.randomUUID();//generates uuid with the crypto package imported
+        const slug = data['title'].split(' ').join('-')//the split will check whenever theres a space(' ') and will insrt('-')to join it and make it a slug
         data['uuid'] = uuid;
         data['slug'] = slug.toLowerCase();
 
